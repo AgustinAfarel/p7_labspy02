11d12d30fbfef0bf0e258be343465f3e40853b0b
pikri ramdani <ramdanifikri74@gmail.com>
2020-11-06 00:08:41 +0700

    input labspy02

---
+++ labspy02.py
@@ -0,0 +1,20 @@
+print("Masukan Angka ke-1 : ")
+angka1=int(input())
+print("Masukan Angka ke-2 : ")
+angka2=int(input())
+print("Masukan Angka ke-3 : ")
+angka3=int(input())
+
+print("\n")
+
+if (angka1 > angka2) and (angka1 > a ngka3) :
+    print(f"Bilangan Pertama Lebih Besar Dari Bilangan Kedua dan Ketiga ")
+
+elif (angka2 > angka1) and (angka2 > angka3) :
+    print(f"Bilangan Kedua Lebih Besar Dari Bilangan Pertama dan Ketiga ")
+
+elif (angka3 > angka1) and (angka3 > angka2) :
+    print("Bilangan ketiga Lebih Besar dari Bilangan Pertama dan Kedua ")
+
+else:
+    print(f"Semua Bilangan Sama Besar")
